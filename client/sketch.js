5a37b1bf037b478159c1fe53e59a10ad5e514798
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2025-09-30 17:37:03 -0700

    Update sketch.js

--- client/sketch.js
+++ client/sketch.js
@@ -44,7 +44,7 @@ socket.on("disconnect", async () => {
     await wait(1000);
     if (socket.connected) return;
   }
-  location.reload();
+  __cpLocation.reload();
 });
 //^^Connects to the socket.io server
 
@@ -546,6 +546,7 @@ CandyDisplay.candy_corn = (function() {
     }
   };
 });
+/*
 CandyDisplay.smarties = (function() {
   var candy = new FitImage("assets/candies/smarties.png");
   return {
@@ -649,7 +650,7 @@ CandyDisplay.blue_candy = (function() {
       tint(255, 255, 255);
     }
   };
-});
+});*/
 
 // Tiles
 var tileIDs = {};
@@ -1641,7 +1642,7 @@ homeDis.clickTutorial = function() {
 homeDis.clickInstructions = function() {
   setDisplay("instructions");
   //cam.scroll = 0;
-  //location.href = location.origin+"/instructions";
+  //__cpLocation.href = __cpLocation.origin+"/instructions";
 }
 homeDis.clickSettings = function() {
   alert("Coming soon!");
@@ -1781,10 +1782,10 @@ function setDisplay(d, sd) {
   subdisplay = sd ?? "";
   DiscordWidget.style.visibility = "hidden";
   user.lastSession = {};
-  history.pushState({}, "", location.origin);
+  history.pushState({}, "", __cpLocation.origin);
   if (display == "lobby") {
-    history.pushState({}, "", location.origin + "?room=" + room);
-    if (isNext) history.pushState({}, "", location.origin + "?room=next");
+    history.pushState({}, "", __cpLocation.origin + "?room=" + room);
+    if (isNext) history.pushState({}, "", __cpLocation.origin + "?room=next");
     return;
   }
   if (display == "home" || display == "loading") return;
@@ -1804,7 +1805,7 @@ function endAssetLoad() {
   }
 
   // Get params
-  var params_str = location.href.split('?')[1];
+  var params_str = __cpLocation.href.split('?')[1];
   if (params_str) {
     var params_arr = params_str.split('&');
 
@@ -2564,9 +2565,9 @@ socket.on('rejoinFailed', returnToHome);
 function returnToHome() {
   user.lastSession = {};
   localStorage.user = JSON.stringify(user);
-  //location.href = location.origin;
+  //__cpLocation.href = __cpLocation.origin;
   setDisplay("home");
-  history.pushState({}, "", location.origin);
+  history.pushState({}, "", __cpLocation.origin);
 }
 socket.on('rejoinSuccess', function(id) {
   player.id = id;
